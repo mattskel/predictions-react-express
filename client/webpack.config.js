d108a1b86822bbdbf61a7e67463074939a90801a
Matt Skelley <mattskelley.au@gmail.com>
2022-10-01 17:06:16 +1000

    Cleanup webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -32,41 +32,9 @@ module.exports = {
           {
             loader: "sass-loader",
             options: {
-              // Prefer `dart-sass`
               implementation: require("sass"),
             },
           },
-          // {
-          //   loader: 'file-loader',
-          //   options: {
-          //     name: 'bundle.css',
-          //   },
-          // },
-          // { loader: 'extract-loader' },
-          // { loader: 'css-loader' },
-          // {
-          //   loader: 'postcss-loader',
-          //   options: {
-          //     postcssOptions: {
-          //       plugins: [
-          //         autoprefixer()
-          //       ]
-          //     }
-          //   } 
-          // },
-          // {
-          //   loader: 'sass-loader',
-          //   options: {
-          //     // Prefer Dart Sass
-          //     implementation: require('sass'),
-
-          //     // See https://github.com/webpack-contrib/sass-loader/issues/804
-          //     webpackImporter: false,
-          //     sassOptions: {
-          //       includePaths: ['./node_modules']
-          //     },
-          //   },
-          // },
         ]
       }
     ]
