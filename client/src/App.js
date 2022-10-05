54fb3226a607de86e0079165d2ef44f75493c4a6
Matt Skelley <mattskelley.au@gmail.com>
2022-10-05 18:24:22 +0800

    Remove class
    Cleanup render.

--- client/src/App.js
+++ client/src/App.js
@@ -8,12 +8,8 @@ import PredictionsForm from "./components/PredictionsForm";
 class App extends Component{
   render(){
     return(
-      <div className="App my-css-class">
-        <h1> Hello, World! </h1>
-        <button className="mdc-button foo-button">
-          <div className="mdc-button__ripple"></div>
-          Button
-        </button>
+      <div className="App">
+        <h1>2023 Predictions</h1>
         <BrowserRouter>
           <Routes>
             <Route path="/predictions/form" element={<PredictionsForm />}/>
